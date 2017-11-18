058ad006337725bb4c5929cb1f84efb6b5c9042c
Rahul Kadukar <rahulkadukar@yahoo.com>
2017-11-18 18:00:47 -0500

    Fixed the logic

--- linearEquations/main.js
+++ linearEquations/main.js
@@ -1,17 +1,41 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
-  let degree = 4;
-  let outputData = prepareArray(degree);
-  outputData.vector = getPoints('1101');
+  let degree = 3;
+  let coeff = degree + 1;
 
-  printData(outputData, 'divInitial');
-  let answerData = coefficients(outputData);
-  printData(answerData, 'divFinal');
+  let data = [];
+  let iterations = Math.pow(2, coeff);
+  for (let x = 0; x < iterations; ++x) {
+    data.push(x.toString(2).padStart(coeff,'0'));
+  }
+
+  let xas;
+
+  for (let x = 0; x < data.length; ++x) {
+    let summaryOutput = [];
+    let outputData = prepareArray(coeff);
+    outputData.vector = getPoints(data[x]);
+    console.log(JSON.parse(JSON.stringify(outputData)));
+    summaryOutput.points = JSON.parse(JSON.stringify(outputData.vector));
+    console.log(summaryOutput);
+    
+    let answerData = coefficients(outputData);
+    summaryOutput.answer = answerData.vector;
+    let finalDivName = 'divFinal' + x.toString();
+    printSummary(summaryOutput, finalDivName);
+    xas = summaryOutput;
+  }
+
+  console.log(xas);
+  paintGraph(xas);  
 });
 
 function getPoints(seed) {
   let data = [];
   for (let x = 0; x < seed.length; ++x) {
     data[x] = Math.floor((Math.random() * 5) + 1);
+    if (seed[x] === '0') {
+      data[x] *= -1;
+    }
   }
   return data;
 }
@@ -81,7 +105,7 @@ function prepareArray(degree) {
     let tempArray = [];
     let sumVector = 0;
     for (let y = degree; y > 0; --y) {
-      let d = Math.pow(x, y);
+      let d = Math.pow(x, y - 1);
       tempArray.push(d + Math.round(Math.random() * randomNum));
       sumVector += (d + Math.round(Math.random() * randomNum));
     }
@@ -89,19 +113,35 @@ function prepareArray(degree) {
     outputData.matrix.push(tempArray);
   }
 
-  console.log(outputData);
-
   return outputData;
 }
 
 function printSummary(output, z) {
-  
+  let x = output.points;
+  let y = output.answer;
+
+  let txt = "$ \\left[\\begin{array}{r|r}";
+  for (let c = 0; c < x.length; ++c) {
+    txt += x[c] + " & " + y[c].toFixed(2);
+
+    if (c + 1 !== x.length) {
+      txt += "\\\\";
+    }
+  }
+
+  txt += "\\end{array}\\right] $";
+
+  let el = document.createElement('div');
+  el.id = z;
+  el.style.float = 'left';
+  document.body.appendChild(el);
+  el.innerHTML = txt;
+  MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
 }
 
 function printData(output, z) {
   let x = output.matrix;
   let y = output.vector;
-  console.log(y);
 
   let txt = "$ \\left[\\begin{array}{rrrr|r}";
   for (let c = 0; c < x.length; ++c) {
@@ -121,4 +161,24 @@ function printData(output, z) {
   el = document.getElementById(z);
   el.innerHTML = txt;
   MathJax.Hub.Queue(["Typeset", MathJax.Hub, el]);
+}
+
+function paintGraph(output) {
+  let x = output.answer;
+  console.log(x);
+
+  let el = document.createElement('svg');
+  el.setAttribute('id', 'svgGraph');
+
+  document.body.appendChild(el);
+
+  for (let c = 1; c <= x.length; ++c) {
+    let d = x.length;
+    let fx = 0;
+    do {
+      fx += (Math.pow(c, d - 1) * x[x.length - d]) ;
+      --d;
+    } while(d > 0);
+    console.log(fx);
+  }
 }
